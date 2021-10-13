5c54f9a6bb9df670e56a1a054f6c098de9911c6e
bindi-v <bindi.vaghela@gmail.com>
2021-10-12 20:53:30 -0400

    added condition in index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const generateHtml = require('./src/generateHtml');
+
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
@@ -5,14 +7,21 @@ const Intern = require('./lib/Intern');
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
-const util = require('util');
+//const path = require('path');
+//const util = require('util');
 
-const mkdirAsync = util.promisify(fs.mkdir);
-writeFileAsync = util.promisify(fs.writeFile);
-const OutputDir = path.resolve(_dirname, 'output');
-const outputPath = path.join(OutputDir, 'team.html');
-const render = require('./lib/htmlRenderer');
+//const mkdirAsync = util.promisify(fs.mkdir);
+//writeFileAsync = util.promisify(fs.writeFile);
+//const OutputDir = path.resolve(_dirname, 'output');
+//const outputPath = path.join(OutputDir, 'team.html');
+//const render = require('./lib/htmlRenderer');
+//const { run } = require{"jest"}
+
+//const teamEmployees = [];
+//const addEmployee = () => {
+   // console.log('Adding  Team Employee : ');
+   // return inquirer.prompt([
+  //  ])
 
 const questions = [
     {
@@ -25,13 +34,60 @@ const questions = [
         message: "What is the Employee's Id?",
 
     }, {
+        type: 'input',
+        name: 'email',
+        message: "What is the Employee's Email?",
+    }, {
         type: 'list',
         name: 'role',
         message: "What is the Employee's role?",
         choices: ['Manager', 'Engineer', 'Intern'],
     }, 
 ];
+//.then(employeeInput => {
+  //let {name, id, email, role, officeNumber, github, school} = employeeInput;
+   // let employee;// = new Manager (name, id, email, officeNumber);
 
+    //teamEmployees.push(manager);
+    //console.log(manager);
+//     if (role === 'Manager') {
+//         employee = new Manager (name, id, email, officeNumber);
+//         console.log(employee);
+//     }else if(role === 'Engineer') {
+//         employee = new Engineer(name, id, email, github);
+//         console.log(employee);
+
+//     }else if(role === 'Intern'){
+//         employee = new Intern (name, id, email, school);
+//         console.log(employee);
+
+//     }
+//     teamEmployees.push(employee);
+//     if(confirmAddEmployee){
+//         return addEmployee(teamEmployees);
+//     }else {
+//         return teamEmployees;
+//     }
+// })
+// };
+
+//const addEmployee = () => {
+   // console.log('Adding  Team Employee : ');
+
+//return inquirer.prompt([
+
+//}
+
+// const writeFile = data => {
+//     fs.writeFile('./dist/index.html', data, err => {
+//         if(err) {
+//             console.log(err);
+//             return " Something Wrong!";
+//         }else {
+//             console.log("Your Team Profile Successfully Generated!")
+//         }
+//     })
+// }
 const questionForManager = [
     {
         type: 'input',
@@ -59,8 +115,61 @@ const questionForIntern = [
 const confirm = [
     {
         type: 'confirm',
-        name: 'addmoreEmployee',
+        name: 'addEmployee',
         message: "Do you want to add more Employee information?",
     }
 ];
 
+const init = async() => {
+    const employees = [];
+    let addMore = true;
+    while(addMore){
+        const {name, id, email, role} = await inquirer.prompt(questions);
+        if(role === 'Manager'){
+            const {officeNumber} = await inquirer.prompt(questionForManager);
+            employees.push(new Manager(name, id, email, officeNumber));
+
+        }else if(role === 'Engineer'){
+            const {github} = await inquirer.prompt(questionForEngineer);
+            employees.push(new Engineer(name, id, email, github));
+
+        }else {
+            const { school} = await inquirer.prompt(questionForIntern);
+            employees.push(new Intern(name, id, email, school));
+
+        }
+        const {addEmployee} = await inquirer.prompt(confirm);
+        addMore = addEmployee;
+    }
+
+    // const html = render(employees);
+    // if(!fs.existsSync(outputPath)){
+    //     const error = await mkdirAsync(OutputDir);
+    //     error && console.log(error);
+
+    //     const error = await writeFileAsync(outputPath, html);
+    //     error && console.log(error);
+    // }
+}
+
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+    if(err){
+        console.log(err);
+        return 'something wrong!';
+    }else {
+        console.log('Your team profile generated!')
+    }
+})
+};
+init()
+.then(addEmployee)
+.then(teamEmployee => {
+    return generateHtml(teamEmployee);
+})
+.then(pageHtml => {
+    return writeFile(pageHtml);
+})
+.catch(err => {
+    console.log(err);
+});
